febd57b37f02b0786397be2ad8f6e033d5024b2e
backad0 <aleshkinalesh1@gmail.com>
2023-12-01 15:10:20 +0600

    bot initialize testing commit

--- src/main/java/com/melnikov/oodbot/OODBot.java
+++ src/main/java/com/melnikov/oodbot/OODBot.java
@@ -1,10 +1,17 @@
 package com.melnikov.oodbot;
 
 import com.melnikov.oodbot.config.BotConfig;
+import com.melnikov.oodbot.model.FunModel;
+import com.melnikov.oodbot.service.FunService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.IOException;
+import java.text.ParseException;
 
 @Component
 @AllArgsConstructor
@@ -24,6 +31,45 @@ public class OODBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        FunModel model = new FunModel();
+        String currency = "";
 
+        if (update.hasMessage() & update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText){
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    try {
+                        currency = FunService.getAnswer(messageText, model);
+
+                    } catch (IOException e) {
+                        sendMessage(chatId, "");
+                    } catch (ParseException e) {
+                        throw new RuntimeException("");
+                    }
+                    sendMessage(chatId, currency);
+            }
+        }
+    }
+
+    private void startCommandReceived(Long chatId, String name) {
+        String answer = "Привет, " + name + ",\n" +
+                "введи ключевое слово\n" +
+                "по которому хочешь вести поиск";
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(Long chatId, String textToSend) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(textToSend);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+        }
     }
 }
--- src/main/java/com/melnikov/oodbot/service/FunService.java
+++ src/main/java/com/melnikov/oodbot/service/FunService.java
@@ -3,10 +3,11 @@ package com.melnikov.oodbot.service;
 import com.melnikov.oodbot.model.FunModel;
 
 import java.io.IOException;
+import java.text.ParseException;
 
 public class FunService {
 
-    public static String getAnswer(String massage, FunModel model) throws IOException{
-        return null;
+    public static String getAnswer(String massage, FunModel model) throws IOException, ParseException {
+        return "пу пу пу";
     }
 }
