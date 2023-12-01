a1f40da1f7720bd60a0f201a13244d162eacbc3d
backad0 <aleshkinalesh1@gmail.com>
2023-12-01 14:46:18 +0600

    Initial commit

---
+++ src/main/java/com/melnikov/oodbot/OodBotApplication.java
@@ -0,0 +1,13 @@
+package com.melnikov.oodbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OodBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OodBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/melnikov/oodbot/OodBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melnikov.oodbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OodBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
